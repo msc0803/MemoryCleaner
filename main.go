74aa8103ea1b799fd6a85aa30264123090ccaf13
mosc <448002701@qq.com>
2026-03-14 11:21:12 +0800

    修复: 添加 ShowWindow/UpdateWindow 和正确的窗口注册，确保窗口显示

--- main.go
+++ main.go
@@ -17,9 +17,10 @@ var (
 	globalMemoryStatus       = kernel32.NewProc("GlobalMemoryStatusEx")
 	setProcessWorkingSetSize = kernel32.NewProc("SetProcessWorkingSetSize")
 	getCurrentProcess        = kernel32.NewProc("GetCurrentProcess")
+	getModuleHandle          = kernel32.NewProc("GetModuleHandleW")
 	createWindowEx           = user32.NewProc("CreateWindowExW")
 	defWindowProc            = user32.NewProc("DefWindowProcW")
-	registerClass            = user32.NewProc("RegisterClassW")
+	registerClassEx          = user32.NewProc("RegisterClassExW")
 	getMessage               = user32.NewProc("GetMessageW")
 	translateMessage         = user32.NewProc("TranslateMessage")
 	dispatchMessage          = user32.NewProc("DispatchMessageW")
@@ -29,6 +30,9 @@ var (
 	beginPaint               = user32.NewProc("BeginPaint")
 	endPaint                 = user32.NewProc("EndPaint")
 	drawText                 = user32.NewProc("DrawTextW")
+	showWindow               = user32.NewProc("ShowWindow")
+	updateWindow             = user32.NewProc("UpdateWindow")
+	loadCursor               = user32.NewProc("LoadCursorW")
 	psapi                    = syscall.NewLazyDLL("psapi.dll")
 )
 
@@ -46,6 +50,11 @@ const (
 	DT_LEFT             = 0x00000000
 	DT_VCENTER          = 0x00000004
 	DT_SINGLELINE       = 0x00000020
+	SW_SHOW             = 5
+	CS_HREDRAW          = 0x0002
+	CS_VREDRAW          = 0x0001
+	IDC_ARROW           = 32512
+	COLOR_WINDOW        = 5
 )
 
 type MEMORYSTATUSEX struct {
@@ -116,22 +125,30 @@ var (
 )
 
 func main() {
+	runtime.LockOSThread()
+	
+	hInstance, _, _ := getModuleHandle.Call(0)
+	hCursor, _, _ := loadCursor.Call(0, uintptr(IDC_ARROW))
+
 	// 注册窗口类
 	wc := WNDCLASSEX{
 		cbSize:        uint32(unsafe.Sizeof(WNDCLASSEX{})),
-		style:         0,
+		style:         CS_HREDRAW | CS_VREDRAW,
 		lpfnWndProc:   syscall.NewCallback(wndProc),
 		cbClsExtra:    0,
 		cbWndExtra:    0,
-		hInstance:     0,
+		hInstance:     hInstance,
 		hIcon:         0,
-		hCursor:       0,
-		hbrBackground: 6, // COLOR_WINDOW+1
+		hCursor:       hCursor,
+		hbrBackground: COLOR_WINDOW + 1,
 		lpszMenuName:  nil,
 		lpszClassName: className,
 		hIconSm:       0,
 	}
-	registerClass.Call(uintptr(unsafe.Pointer(&wc)))
+	ret, _, _ := registerClassEx.Call(uintptr(unsafe.Pointer(&wc)))
+	if ret == 0 {
+		panic("Failed to register window class")
+	}
 
 	// 创建窗口
 	windowTitle := syscall.StringToUTF16Ptr("Memory Cleaner - 内存清理工具 by mosc")
@@ -139,41 +156,55 @@ func main() {
 		0,
 		uintptr(unsafe.Pointer(className)),
 		uintptr(unsafe.Pointer(windowTitle)),
-		WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		450, 400,
-		0, 0, 0, 0,
+		0, 0, hInstance, 0,
 	)
+	
+	if hwnd == 0 {
+		panic("Failed to create window")
+	}
+
+	// 显示窗口
+	showWindow.Call(hwnd, SW_SHOW)
+	updateWindow.Call(hwnd)
+
+	// 显示窗口
+	showWindow.Call(hwnd, SW_SHOW)
+	updateWindow.Call(hwnd)
 
 	// 创建按钮
+	btnClass := syscall.StringToUTF16Ptr("BUTTON")
+	
 	btn1Text := syscall.StringToUTF16Ptr("清理物理内存")
 	hwndBtn1, _, _ = createWindowEx.Call(
 		0,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("BUTTON"))),
+		uintptr(unsafe.Pointer(btnClass)),
 		uintptr(unsafe.Pointer(btn1Text)),
 		WS_VISIBLE|WS_CHILD|BS_PUSHBUTTON|WS_TABSTOP,
 		50, 200, 150, 40,
-		hwnd, 1, 0, 0,
+		hwnd, 1, hInstance, 0,
 	)
 
 	btn2Text := syscall.StringToUTF16Ptr("清理工作集")
 	hwndBtn2, _, _ = createWindowEx.Call(
 		0,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("BUTTON"))),
+		uintptr(unsafe.Pointer(btnClass)),
 		uintptr(unsafe.Pointer(btn2Text)),
 		WS_VISIBLE|WS_CHILD|BS_PUSHBUTTON|WS_TABSTOP,
 		230, 200, 150, 40,
-		hwnd, 2, 0, 0,
+		hwnd, 2, hInstance, 0,
 	)
 
 	btn3Text := syscall.StringToUTF16Ptr("启动自动清理")
 	hwndBtn3, _, _ = createWindowEx.Call(
 		0,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("BUTTON"))),
+		uintptr(unsafe.Pointer(btnClass)),
 		uintptr(unsafe.Pointer(btn3Text)),
 		WS_VISIBLE|WS_CHILD|BS_PUSHBUTTON|WS_TABSTOP,
 		100, 260, 230, 40,
-		hwnd, 3, 0, 0,
+		hwnd, 3, hInstance, 0,
 	)
 
 	// 设置定时器更新显示 (每2秒)
